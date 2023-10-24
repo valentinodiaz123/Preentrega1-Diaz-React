34eba9651d7ed2b1d0d30843df913461badaf016
Valentino Diaz <valentinodiaz0303@gmail.com>
2023-10-23 23:54:36 -0300

    Grid

--- pre-entrega/src/components/item/item.jsx
+++ pre-entrega/src/components/item/item.jsx
@@ -4,12 +4,15 @@ const Item = ({ productos }) => {
     return (
         <>
             <div className="contenedor-item-list" >
+                
                 <img className="image-item-list" src={productos.image} alt="" />
+
                 <div className="text-item-list">
                     <h1 className="card-title">{productos.title}</h1>
                     <p className="card-price">{productos.price}</p>
-                    <button className="btn btn-primary m-3"><Link className="text-light text-decoration-none"to={`/item/${productos.id}`}>Ver Mas</Link></button>
                 </div>
+
+                <button className="btn btn-primary m-3"><Link className="text-light text-decoration-none"to={`/item/${productos.id}`}>Ver Mas</Link></button>
 
             </div>
         </>)
